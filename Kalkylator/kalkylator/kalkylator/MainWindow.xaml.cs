9113a84757e0e3b09b8b9a7a2b1ddbfaa3f39fdb
mjyyy1 <malte.erlandsson@elev.ga.ntig.se>
2020-11-13 15:25:30 +0100

    buttun work nu

--- Kalkylator/kalkylator/kalkylator/MainWindow.xaml.cs
+++ Kalkylator/kalkylator/kalkylator/MainWindow.xaml.cs
@@ -15,22 +15,144 @@ using System.Windows.Shapes;
 
 namespace kalkylator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        string input;//turn this to calc1 before asking for next number
+        int convert;
+        double calc1;
+        double calc2;
+        bool isnegative = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+
+        void Noll(object sender, RoutedEventArgs e)
+        {
+
+            input += "0";
+            Output.Text = input;
+        }
+        void One(object sender, RoutedEventArgs e)
+        {
+
+            input += "1";
+            Output.Text = input;
+        }
+
+        void Two(object sender, RoutedEventArgs e)
+        {
+
+            input += "2";
+            Output.Text = input;
+        }
+        void Three(object sender, RoutedEventArgs e)
+        {
+
+            input += "3";
+            Output.Text = input;
+        }
+        void Four(object sender, RoutedEventArgs e)
+        {
+
+            input += "4";
+            Output.Text = input;
+        }
+        void Five(object sender, RoutedEventArgs e)
+        {
+
+            input += "5";
+            Output.Text = input;
+        }
+        void Six(object sender, RoutedEventArgs e)
+        {
+
+            input += "6";
+            Output.Text = input;
+        }
+        void Seven(object sender, RoutedEventArgs e)
+        {
+
+            input += "7";
+            Output.Text = input;
+        }
+        void Eight(object sender, RoutedEventArgs e)
+        {
+
+            input += "8";
+            Output.Text = input;
+        }
+        void Nine(object sender, RoutedEventArgs e)
+        {
+
+            input += "9";
+            Output.Text = input;
+        }
+
+
+
+        void PlusMinus(object sender, RoutedEventArgs e)
+        {
+            if (isnegative == false)
+
+            {
+               
+            }
+            else if (isnegative == true){
+
+            }
+        }
+        void Comma(object sender, RoutedEventArgs e)
+        {
+
+            input += ",";
+            Output.Text = input;
+        }
+        void Equals(object sender, RoutedEventArgs e)
+        {
+            calc2 = Convert.ToDouble(input);
+            input = "";
+            Output.Text = input;
+        }
+        void Plus(object sender, RoutedEventArgs e)
+        {
+            calc1 = Convert.ToDouble(input);
+            input = "";
+            Output.Text = input;
+        }
+
+        void Minus(object sender, RoutedEventArgs e)
+        {
+
+        }
+        void Multiplication(object sender, RoutedEventArgs e)
+        {
+
+        }
+        void Division(object sender, RoutedEventArgs e)
+        {
+
+        }
+        void CE(object sender, RoutedEventArgs e)
+        {
+
+        }
+        void C(object sender, RoutedEventArgs e)
+        {
+
+        }
+        void Del(object sender, RoutedEventArgs e)
+        {
+         
             
         }
 
-      
 
-        void noll(object sender, RoutedEventArgs e)
-        {
-            Output.Text = "bababooey ";
-        }
-    }
+
+}
+
 }
