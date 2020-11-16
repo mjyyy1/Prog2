fbc7b004ce9c4d6994e791bed899362e09b9b0f8
mjyyy1 <malte.erlandsson@elev.ga.ntig.se>
2020-11-16 12:58:16 +0100

    it works

--- Kalkylator/kalkylator/kalkylator/MainWindow.xaml.cs
+++ Kalkylator/kalkylator/kalkylator/MainWindow.xaml.cs
@@ -17,12 +17,17 @@ namespace kalkylator
 {
     public partial class MainWindow : Window
     {
-
-        string input;//turn this to calc1 before asking for next number
-        int convert;
+        string input;
         double calc1;
         double calc2;
-        bool isnegative = false;
+        double ismin;
+        double sum;
+        bool plus = false;
+        bool min = false;
+        bool multip = false;
+        bool div = false;
+
+        bool usedcomma = false;
 
         public MainWindow()
         {
@@ -97,57 +102,218 @@ namespace kalkylator
 
         void PlusMinus(object sender, RoutedEventArgs e)
         {
-            if (isnegative == false)
 
+            if (String.IsNullOrEmpty(input))
             {
-               
-            }
-            else if (isnegative == true){
 
             }
+            else
+            {
+
+
+                calc1 = Convert.ToDouble(input);
+                ismin = calc1 * -1;
+
+                input = Convert.ToString(ismin);
+
+
+            }
+            Output.Text = input;
         }
         void Comma(object sender, RoutedEventArgs e)
         {
-
-            input += ",";
-            Output.Text = input;
+            if (usedcomma == false)
+            {
+                input += ",";
+                Output.Text = input;
+                usedcomma = true;
+            }
+            
         }
         void Equals(object sender, RoutedEventArgs e)
         {
             calc2 = Convert.ToDouble(input);
             input = "";
+            if (plus == true)
+            {
+                sum = calc1 + calc2;
+            }
+            else if (min == true)
+            {
+
+                if (calc1 >= calc2)
+                {
+                    sum = calc1 - calc2;
+                }
+                else
+                {
+                    sum = 000;
+                }
+                
+            }
+            else if (multip == true)
+            {
+                sum = calc1 * calc2;
+            }
+            else if (div == true)
+            {
+                if (calc2 != 0)
+                {
+                    sum = calc1 / calc2;
+                }
+                else
+                {
+                    sum = 000;
+                }
+
+               
+            }
+
+            input = Convert.ToString(sum);
             Output.Text = input;
+            plus = false;
+            min = false;
+            div = false;
+            multip = false;
+            usedcomma = false;
+            input = "";
         }
         void Plus(object sender, RoutedEventArgs e)
         {
-            calc1 = Convert.ToDouble(input);
-            input = "";
-            Output.Text = input;
+            if (String.IsNullOrEmpty(input))
+            {
+
+            }
+            else
+            {
+
+
+                if (multip || div || min)
+                {
+
+                }
+                else
+                {
+                    calc1 = Convert.ToDouble(input);
+                    input = "";
+                    Output.Text = input;
+                    plus = true;
+                    usedcomma = false;
+                }
+
+            }
         }
 
         void Minus(object sender, RoutedEventArgs e)
         {
+            if (String.IsNullOrEmpty(input))
+            {
 
+            }
+            else
+            {
+
+
+                if (multip || div || plus)
+                {
+
+                }
+                else
+                {
+                    calc1 = Convert.ToDouble(input);
+                    input = "";
+                    Output.Text = input;
+                    min = true;
+                    usedcomma = false;
+                }
+            }
+            
         }
         void Multiplication(object sender, RoutedEventArgs e)
         {
+            if (String.IsNullOrEmpty(input))
+            {
 
+            }
+            else
+            {
+
+
+                if (plus || div || min)
+                {
+
+                }
+                else
+                {
+                    calc1 = Convert.ToDouble(input);
+                    input = "";
+                    Output.Text = input;
+                    multip = true;
+                    usedcomma = false;
+                }
+            }
+            
         }
         void Division(object sender, RoutedEventArgs e)
         {
+            if (String.IsNullOrEmpty(input))
+            {
 
+            }
+            else
+            {
+
+
+
+                if (multip || plus || min)
+                {
+
+                }
+                else
+                {
+                    calc1 = Convert.ToDouble(input);
+                    input = "";
+                    Output.Text = input;
+                    div = true;
+                    usedcomma = false;
+                }
+            }
+            
         }
         void CE(object sender, RoutedEventArgs e)
         {
-
+            calc1 = 0;
+            calc2 = 0;
+            input = "";
+            Output.Text = input;
         }
         void C(object sender, RoutedEventArgs e)
         {
-
+            if (plus||multip||div||min)
+            {
+                calc2 = 0;
+                
+            }
+            else
+            {
+                calc1 = 0;
+            }
+            input = "";
+            Output.Text = input;
         }
         void Del(object sender, RoutedEventArgs e)
         {
-         
+
+            if (String.IsNullOrEmpty(input))
+            {
+
+            }
+            else
+            {
+                string f = input.Remove(input.Length - 1);
+                input = f;
+                Output.Text = input;
+            }
             
         }
 
