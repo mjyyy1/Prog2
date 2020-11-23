9165dabfc4bd3422a7a7b337bfe36dcd53d4638c
mjyyy1 <54800274+mjyyy1@users.noreply.github.com>
2020-11-23 11:59:53 +0100

    m

--- Kalkylator/kalkylator/kalkylator/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Kalkylator/kalkylator/kalkylator/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace kalkylator {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace kalkylator {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public static void Main() {
             kalkylator.App app = new kalkylator.App();
             app.InitializeComponent();
