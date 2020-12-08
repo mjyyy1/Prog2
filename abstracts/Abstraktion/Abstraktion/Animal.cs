c33275dc7e82dc27a8c3fc0e970539b3c3a4d271
mjyyy1 <54800274+mjyyy1@users.noreply.github.com>
2020-12-08 09:55:06 +0100

    grs

---
+++ abstracts/Abstraktion/Abstraktion/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraktion
+{
+    abstract class Animal
+    {
+        public int NumberOfLimbs { get; set; }
+        public string Color { get; set; }
+
+        public abstract void Breathe();
+
+        public abstract void Rest();
+        public abstract void Eat();
+
+    }
+}
---
+++ abstracts/Abstraktion/Abstraktion/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraktion
+{
+    class Dog : Animal
+    {
+
+        public Dog()
+        {
+                
+        }
+        public override void Breathe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Rest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
