42fcc5997bf7d39a62f6afacff8abf9dbb74cd21
mjyyy1 <54800274+mjyyy1@users.noreply.github.com>
2021-01-25 12:48:57 +0100

    dasd

---
+++ solidlabbs/SOLID-Labb/SOLID-Labb/Dog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Labb
+{
+    class Dog : Animal
+    {
+
+        public Dog(string color): base(color)
+        {
+            
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Dog is barking!");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("Dog is sleeping!");
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Dog is eating. " + _owner +" is happy!");
+        }
+
+
+    }
+}
---
+++ solidlabbs/SOLID-Labb/SOLID-Labb/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Labb
+{
+    public abstract class Human
+    {
+        public string _purchase;
+        public string _animal;
+
+        public List<Human> pets = new List<Human>();
+
+        public String Animal
+        {
+            get { return _animal; }
+            set { _animal = value; }
+        }
+
+
+        public void BuyAnimal(string Purchase)
+            {
+                _purchase = Purchase;
+            }
+        
+    }
+}
